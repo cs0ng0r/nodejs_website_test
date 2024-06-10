98c609a43577461361b9000290022919d2a063d4
Cs0ng0r <bikadicsongor@gmail.com>
2024-06-10 19:27:11 +0200

    Translated to hungarian

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const http = require("http");
-const fs = require("fs");
-const port = 3000;
+const http = require("http"); // HTTP modul
+const fs = require("fs"); // File System modul
+const port = 3000; // Általunk választott port
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   fs.readFile("index.html", function (error, data) {
     if (error) {
-      res.writeHead(404);
-      res.write("Error: File not found");
+      res.writeHead(404); // 404-es hibakód
+      res.write("Error: Nem találtam meg ezt a filet."); // Hibaüzenet, ha nem találja a fájlt
     } else {
-      res.write(data);
+      res.write(data); // Visszaadja a fájlt, ha megtalálta és nincs hiba
     }
     res.end();
   });
@@ -17,8 +17,8 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, function (error) {
   if (error) {
-    console.log("Something went wrong", error);
+    console.log("Valami nem stimmel", error); // Hibaüzenet, ha valami nem stimmel
   } else {
-    console.log("Server is listening on port " + port);
+    console.log("A szerver az alábbi porton fut: " + port);  // Sikeres indítás esetén kiírja a portot
   }
 });
